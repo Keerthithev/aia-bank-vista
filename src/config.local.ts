333d6230c223c4c49c9688c8a499a90a11dbd657
Keerthithev <keerthiganthevarasa@gmail.com>
2025-07-24 22:18:33 +0530

    changes

--- src/config.local.ts
+++ src/config.local.ts
@@ -1,3 +1,2 @@
 // Local config for secrets. DO NOT COMMIT THIS FILE.
-export const GROQ_API_KEY = import.meta.env.VITE_GROQ_API_KEY;
-console.log("GROQ_API_KEY in production:", import.meta.env.VITE_GROQ_API_KEY);
+export const GROQ_API_KEY = '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'; 
